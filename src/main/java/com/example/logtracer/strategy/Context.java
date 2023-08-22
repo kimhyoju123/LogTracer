68388e682431510e8bae5c4ef87d7205d01898d2
kimhyoju123 <qjqmf217@naver.com>
2023-08-22 15:19:59 +0900

    Add comment

--- src/main/java/com/example/logtracer/strategy/Context.java
+++ src/main/java/com/example/logtracer/strategy/Context.java
@@ -4,6 +4,8 @@ import com.example.logtracer.TraceStatus;
 import com.example.logtracer.logtrace.LogTrace;
 import lombok.RequiredArgsConstructor;
 
+// Context 내부에 Strategy 필드를 가지고 있으며, 이 필드에 변하는 부분인 Strategy 의 구현체를 주입하면 된다.
+
 @RequiredArgsConstructor
 public class Context<T> {
 
